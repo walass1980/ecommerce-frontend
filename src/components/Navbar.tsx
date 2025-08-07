53142ad1b50a5eb82686db53171173af8aa18861
walass <cha_wal100@yahoo.fr>
2025-08-07 17:08:05 +0200

    up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,24 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const Navbar = () => {
   return (
-    <div className=''>Navbar</div>
+    <nav>
+        {/* LEFT */}
+        <Link href="/" className="flex item-center">
+         <Image 
+         src="/logo.png" 
+         alt="TrendLama" 
+         width={36} 
+         height={36} 
+         className="w-6 h-6 md:w-9"
+         />
+         <p className="text-md font-medium">
+            TRENDWALASS
+         </p>
+        </Link>
+        {/* RIGHT */}
+    </nav>
   )
 }
 
