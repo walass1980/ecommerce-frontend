aa94685b9418760844a777266fe122b1bde34c83
walass <cha_wal100@yahoo.fr>
2025-08-09 13:18:43 +0200

    up

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -57,8 +57,14 @@ import {
 const Categories = () => {
  const searchParams = useSearchParams()
  const router = useRouter()
+ const pathName = usePathname()
  const selectedCategory = searchParams.get("category")
-  console.log(selectedCategory)
+ 
+ const handleChange = (value: string | null) => {
+     const params = new URLSearchParams(searchParams)
+     params.set("category", value || "all")
+     router.push(`${pathName}?${params.toString()}`, {scroll: false})
+ }
 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg-gray-100 p-2 rounded-lg mb-4 text-sm">
@@ -66,13 +72,15 @@ const Categories = () => {
             <div className={`flex items-center justify-center gap-2 cursor-pointer px-2 py-1 rounded-md ${
                 category.slug === selectedCategory ? "bg-white" : "text-gray-500"
             }`} 
-            key={category.name}>
+            key={category.name}
+            onClick={() => handleChange(category.slug)}
+            >
                 {category.icon}
                 {category.name}
             </div>
         ))}
     </div>
-  
+  )
 }
 
 export default Categories
