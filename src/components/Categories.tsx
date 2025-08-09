eefb62727a729eb75a70764cb0544d0481be8456
walass <cha_wal100@yahoo.fr>
2025-08-09 03:16:30 +0200

    up

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -55,8 +55,23 @@ import {
   ];
 
 const Categories = () => {
+ const searchParams = useSearchParams()
+ const router = useRouter()
+ const selectedCategory = searchParams.get("category")
+  console.log(selectedCategory)
+
   return (
-    <div>Categories</div>
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg-gray-100 p-2 rounded-lg mb-4 text-sm">
+        {categories.map(category => (
+            <div className={`flex items-center justify-center gap-2 cursor-pointer px-2 py-1 rounded-md ${
+                category.slug === selectedCategory ? "bg-white" : "text-gray-500"
+            }`} 
+            key={category.name}>
+                {category.icon}
+                {category.name}
+            </div>
+        ))}
+    </div>
   )
 }
 
