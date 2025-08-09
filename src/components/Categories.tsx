3734c87a45e8adfca600c39bee88e89fcd981940
walass <cha_wal100@yahoo.fr>
2025-08-09 02:40:59 +0200

    up

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,5 +1,59 @@
 "use client"
 
+import {
+    Footprints,
+    Glasses,
+    Briefcase,
+    Shirt,
+    ShoppingBasket,
+    Hand,
+    Venus,
+  } from "lucide-react";
+  import { usePathname, useRouter, useSearchParams } from "next/navigation";
+  
+  const categories = [
+    {
+      name: "All",
+      icon: <ShoppingBasket className="w-4 h-4" />,
+      slug: "all",
+    },
+    {
+      name: "T-shirts",
+      icon: <Shirt className="w-4 h-4" />,
+      slug: "t-shirts",
+    },
+    {
+      name: "Shoes",
+      icon: <Footprints className="w-4 h-4" />,
+      slug: "shoes",
+    },
+    {
+      name: "Accessories",
+      icon: <Glasses className="w-4 h-4" />,
+      slug: "accessories",
+    },
+    {
+      name: "Bags",
+      icon: <Briefcase className="w-4 h-4" />,
+      slug: "bags",
+    },
+    {
+      name: "Dresses",
+      icon: <Venus className="w-4 h-4" />,
+      slug: "dresses",
+    },
+    {
+      name: "Jackets",
+      icon: <Shirt className="w-4 h-4" />,
+      slug: "jackets",
+    },
+    {
+      name: "Gloves",
+      icon: <Hand className="w-4 h-4" />,
+      slug: "gloves",
+    },
+  ];
+
 const Categories = () => {
   return (
     <div>Categories</div>
