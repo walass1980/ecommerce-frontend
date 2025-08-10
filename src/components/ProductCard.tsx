a55360be1c6aa18f1e72a5086f29b4020fc29b01
walass <cha_wal100@yahoo.fr>
2025-08-10 14:13:01 +0200

    up

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,10 +1,21 @@
 "use client"
 
 import { ProductType } from "@/types"
+import Image from "next/image"
 
 const ProductCard = ({product}: {product: ProductType}) => {
   return (
-    <div>ProductCard</div>
+    <div className="shadow-lg rounded-lg overflow-hidden">
+      {/* IMAGE */}
+      <div className="className">
+        <Image 
+        src={product.images[ProductTypes]}
+        alt={product.name}
+        fill
+        className="object-cover hover:scale-105 transition-all duration-300"
+        />
+      </div>
+    </div>
   )
 }
 
