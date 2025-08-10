418b3d71ca9d3b3b43e022bf5fa85e566f8083de
walass <cha_wal100@yahoo.fr>
2025-08-10 14:22:14 +0200

    up

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,14 +2,20 @@
 
 import { ProductType } from "@/types"
 import Image from "next/image"
+import { useState } from "react"
 
 const ProductCard = ({product}: {product: ProductType}) => {
+  const [productType, setProductType] = useState({
+     size: product.sizes[0],
+     color: product.colors[0]
+  })
+  
   return (
     <div className="shadow-lg rounded-lg overflow-hidden">
       {/* IMAGE */}
-      <div className="className">
+      <div className="relative aspect-[2/3]">
         <Image 
-        src={product.images[ProductTypes]}
+        src={product.images[productType.color]}
         alt={product.name}
         fill
         className="object-cover hover:scale-105 transition-all duration-300"
