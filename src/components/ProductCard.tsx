97abeefdb9fe42e7cf1b2fb983115949901ac3d5
walass <cha_wal100@yahoo.fr>
2025-08-10 22:29:35 +0200

    up

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -46,7 +46,23 @@ const ProductCard = ({product}: {product: ProductType}) => {
         {/* COLORS */}
         <div className="flex flex-col gap-1">
           <span className="text-gray-500">Color</span>
-          
+          <div className="flex items-center gap-2">
+            {product.colors.map((color) => (
+              <div
+              className={`cursor-pointer border-1 ${
+                productType.color == color
+                ? "border-gray-400"
+                : "border-gray-200"
+              } rounded-full p-[1.2]`}
+              key={color}
+              >
+                <div
+                 className="w-[14px] h-[14px] roiunded-full"
+                 style={{ backgroundColor: color }}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
