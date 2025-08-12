7fafdbb42abbf92ab7557815caa36ee40600a254
walass <cha_wal100@yahoo.fr>
2025-08-13 00:32:56 +0200

    up

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -5,6 +5,12 @@ const Filter = () => {
     const searchParams = useSearchParams()
     const router = useRouter()
     const pathname = usePathname()
+
+    const handleFilter = (value: string) => {
+        const params = new URLSearchParams(searchParams)
+        params.set("sort", value)
+        router.push(`${pathname}?${params.toString()}`, { scroll: false })
+    }
   return (
     <div className="flex items-center justify-end gap-2 text-sm text-gray-500 my-6">
         <span>Srted by:</span>
@@ -12,6 +18,7 @@ const Filter = () => {
          name="sort" 
          id="sort"
          className="ring-1 ring-gray-200 shadow-md p-1 rounded-sm"
+         onChange={(e) => handleFilter(e.target.value)}
          >
             <option value="newest">Newest</option>
             <option value="oldest">Oldest</option>
