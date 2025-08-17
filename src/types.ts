f8b119104d65e8b5afb8447a9cdd35f84b0f15d4
walass <cha_wal100@yahoo.fr>
2025-08-17 23:40:30 +0200

    up

--- src/types.ts
+++ src/types.ts
@@ -29,6 +29,8 @@ export const shippingFormSchema = z.object({
      .min(7, "Phone must be between 7 and 10 digits!")
      .max(10, "Phone number must be between 7 and 10 digits!")
      .regex(/^\d+$/, "Phone number must contain only numbers!"),
+  address: z.string().min(1, "Address is required!"),
+  city: z.string().min(1, "City is required!"),
 })
 
 export type CartStoreStateType = {
