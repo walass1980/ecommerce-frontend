9b52fc620dfae242b80869f815703ec3ab2468da
walass <cha_wal100@yahoo.fr>
2025-08-17 23:37:28 +0200

    up

--- src/types.ts
+++ src/types.ts
@@ -24,6 +24,11 @@ export type CartsItemType = CartItemType[]
 export const shippingFormSchema = z.object({
   name: z.string().min(1, "Name is required!"),
   email: z.email().min(1, "Email is required!"),
+  Phone: z
+     .string()
+     .min(7, "Phone must be between 7 and 10 digits!")
+     .max(10, "Phone number must be between 7 and 10 digits!")
+     .regex(/^\d+$/, "Phone number must contain only numbers!"),
 })
 
 export type CartStoreStateType = {
