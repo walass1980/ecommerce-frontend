d7753a25ad85a3e52bfd3e06384c493439b2d359
walass <cha_wal100@yahoo.fr>
2025-08-21 00:26:26 +0200

    up

--- src/components/ShoppingCartIcon.tsx
+++ src/components/ShoppingCartIcon.tsx
@@ -1,15 +1,21 @@
-"use client"
+"use client";
 
-import { ShoppingCart } from "lucide-react"
-import Link from "next/link"
+import useCartStore from "@/stores/cartStore";
+import { ShoppingCart } from "lucide-react";
+import Link from "next/link";
 
 const ShoppingCartIcon = () => {
+  const { cart, hasHydrated } = useCartStore();
+
+  if (!hasHydrated) return null;
   return (
     <Link href="/cart" className="relative">
-        <ShoppingCart className="w-4 h-4 text-gray-600"/>
-        <span className="absolute -top-3 -right-3 bg-amber-400 text-gray-600 rounded-full w-4 h-4 flex items-center justify-center text-sm font-medium">0</span>
+      <ShoppingCart className="w-4 h-4 text-gray-600" />
+      <span className="absolute -top-3 -right-3 bg-amber-400 text-gray-600 rounded-full w-4 h-4 flex items-center justify-center text-xs font-medium">
+        {cart.reduce((acc, item) => acc + item.quantity, 0)}
+      </span>
     </Link>
-  )
-}
+  );
+};
 
-export default ShoppingCartIcon
+export default ShoppingCartIcon;
