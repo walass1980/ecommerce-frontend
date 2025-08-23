75d4720d9adf9ed524e13d2e54db0bec8f73004f
walass <cha_wal100@yahoo.fr>
2025-08-23 22:20:59 +0200

    up

--- admin/src/app/users/page.tsx
+++ admin/src/app/users/page.tsx
@@ -1,3 +1,6 @@
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
+
 const getData = async (): Promise<User[]> => {
   return [
     {
@@ -258,8 +261,11 @@ const getData = async (): Promise<User[]> => {
 const UsersPage = async() => {
   const data = await getData()
   return (
-    <div className=''>
-      <h1 className="font-semibold">All Users</h1>
+    <div className="">
+      <div className='mb-8 px-4 py-2 bg-secondary rounded-md'>
+        <h1 className="font-semibold">All Users</h1>
+      </div>
+      <DataTable columns={columns} data={data}/>
     </div>
   )
 }
