c5c5b0f5725bb50c40c83d36a6fd04b780a2278d
Mac <mac@imac-de-mac.home>
2025-08-25 11:01:40 +0200

    up

--- admin/src/app/payments/page.tsx
+++ admin/src/app/payments/page.tsx
@@ -295,13 +295,13 @@ const getData = async (): Promise<Payment[]> => {
 };
 
 const PaymentsPage = async () => {
-  const data = await getData();
+  const data = await getData()
   return (
     <div className="">
-      <div className="mb-8 px-4 py-2 bg-secondary rounded-md">
-        <h1 className="font-semibold">All Payments</h1>
-      </div>
-      <DataTable columns={columns} data={data}/>
+       <div className="">
+        <h1 className="mb-8 px-4 py-2 bg-secondary rounded-md">All payments</h1>
+       </div>
+       <DataTable columns={columns} data={data}/>
     </div>
   );
 };
